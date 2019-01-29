e195ba27cb41827b739b9af8131ce1ea81fbdbfd
nadira.nuri <nadira.nuri@gmail.com>
2019-01-29 18:21:51 +0600

    t 3 20190129

--- www/survey.js
+++ www/survey.js
@@ -1545,8 +1545,8 @@ function check_user() {
 	
 	
 	
-	var  apipath_base_photo_dm='http://w05.yeapps.com/mundi/syncmobile_417_new_ibn_newtest_web/dmpath?CID='+cid +'&HTTPPASS=e99business321cba'
-	//var  apipath_base_photo_dm='http://w02.yeapps.com/welcome/dmpath_live_web/get_path?CID='+cid +'&HTTPPASS=e99business321cba'
+	//var  apipath_base_photo_dm='http://w05.yeapps.com/mundi/syncmobile_417_new_ibn_newtest_web/dmpath?CID='+cid +'&HTTPPASS=e99business321cba'
+	var  apipath_base_photo_dm='http://w02.yeapps.com/welcome/dmpath_live_web/get_path?CID='+cid +'&HTTPPASS=e99business321cba'
 	
 	
 	//localStorage.path_value_tour='http://w05.yeapps.com/kpl/tour_web/'
